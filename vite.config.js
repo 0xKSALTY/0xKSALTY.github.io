d3443d28f960212936fb833173462728e0f898e0
root <root@debian-12.deb-12>
2024-08-16 20:36:38 +0100

    configuring threejs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/0xKSALTY.github.io/dist/', 
+  root: 'dist',  // Set the root to 'dist'
+  build: {
+    outDir: '../build',  // Output to a different folder to avoid conflicts
+    rollupOptions: {
+      input: resolve(__dirname, 'dist/index.html'),
+    },
+  },
+  server: {
+    historyApiFallback: true,  // Enable SPA fallback
+  },
 });
 
