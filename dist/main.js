3d3e5f34a3490bdeb229eb9358f45c7b99e53793
root <root@debian-12.deb-12>
2024-08-26 20:27:03 +0100

    updated cdn

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.152.2/examples/jsm/controls/OrbitControls.js';
 import { gsap } from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.js';
 
 // rendering and scene
